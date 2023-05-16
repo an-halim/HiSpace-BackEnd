0cdb2ba30d4ea5b12817bd721016e736a6284414
an-halim <halimbla2@gmail.com>
2023-05-16 23:48:41 +0700

    fix: database connection option

--- src/database/index.js
+++ src/database/index.js
@@ -1,30 +1,25 @@
-import Sequelize from 'sequelize';
-import dotenv from 'dotenv';
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const {
-  DB_HOST,
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_NAME,
-  DB_DIALECT,
-  DB_PORT,
-  APP_ENV,
+	DB_HOST,
+	DB_USERNAME,
+	DB_PASSWORD,
+	DB_NAME,
+	DB_DIALECT,
+	DB_PORT,
+	APP_ENV,
 } = process.env;
 
 const db = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL, {
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true,
-      },
-    },
-  }) : new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-    host: DB_HOST,
-    dialect: DB_DIALECT,
-    port: DB_PORT,
-    logging: APP_ENV === 'development' ? console.log : false,
-  });
+	? new Sequelize(process.env.DB_URL, {})
+	: new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+			host: DB_HOST,
+			dialect: DB_DIALECT,
+			port: DB_PORT,
+			logging: APP_ENV === "development" ? console.log : false,
+	  });
 
 export default db;
