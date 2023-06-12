f3cd55caff71aa2275dbbff89dd08f54d4bba71b
an-halim <halimbla2@gmail.com>
2023-06-12 11:40:12 +0700

    refactor: add validation for file upload

--- src/middlewares/formidable.js
+++ src/middlewares/formidable.js
@@ -1,15 +1,46 @@
 import formidable from "formidable";
 
 export default (req, res, next) => {
-  const form = formidable({ multiples: true });
+	const form = formidable({ multiples: true });
 
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    req.body = fields;
-    req.files = files;
-    next();
-  });
-}
+	form.parse(req, (err, fields, files) => {
+		if (err) {
+			next(err);
+			return;
+		}
+		// if file extension is not an image
+		if (files.image) {
+			const { type } = files.image;
+			const split = type.split("/");
+			const extension = split[1].toLowerCase();
+
+			if (extension !== "png" && extension !== "jpg" && extension !== "jpeg") {
+				const error = {
+					message: "File type is not supported",
+					code: "UNSUPPORTED_FILE",
+				};
+				return res.status(400).json({
+					status: "failed",
+					error: error,
+				});
+			} else if (files.image.size > 1000000) {
+				const error = {
+					message: "File size is too large",
+					code: "FILE_SIZE_TOO_LARGE",
+				};
+				return res.status(400).json({
+					status: "failed",
+					error: error,
+				});
+			} else {
+				req.body = fields;
+				req.files = files;
+				next();
+			}
+		} else {
+			req.body = fields;
+			req.files = files;
+			next();
+		}
+	});
+};
