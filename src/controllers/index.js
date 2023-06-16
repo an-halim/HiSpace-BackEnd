5026b00d157e7072c4775a0215c17fae2c7b4b29
an-halim <halimbla2@gmail.com>
2023-06-17 06:35:23 +0700

    refactor: import in one entry point

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,5 +2,14 @@ import userController from "./user.controller.js";
 import locationController from "./location.controller.js";
 import reviewController from "./review.controller.js";
 import wishListController from "./wishlist.controller.js";
+import menuController from "./menu.controller.js";
+import facilityController from "./facility.controller.js";
 
-export { userController, locationController, reviewController, wishListController };
+export {
+	userController,
+	locationController,
+	reviewController,
+	wishListController,
+	menuController,
+	facilityController,
+};
