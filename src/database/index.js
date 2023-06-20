d57baa6bcfa643eab22de9fabb7bc30da925a028
an-halim <halimbla2@gmail.com>
2023-06-20 18:31:19 +0700

    fix: database logging on production

--- src/database/index.js
+++ src/database/index.js
@@ -14,7 +14,9 @@ const {
 } = process.env;
 
 const db = process.env.DB_URL
-	? new Sequelize(process.env.DB_URL, {})
+	? new Sequelize(process.env.DB_URL, {
+			logging: APP_ENV === "development" ? console.log : false,
+	  })
 	: new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
 			host: DB_HOST,
 			dialect: DB_DIALECT,
